d7bac86c83ca5bfda007f2513fba4cf119aebd38
Pr0igroman <ilya-shuruhin@yandex.ru>
2024-02-19 02:26:04 +0300

    HomeWork lesson 26

---
+++ HomeWork_Shurukhin/HW_26_lesson.py
@@ -0,0 +1,19 @@
+num_student = int(input("Введите количество студентов: "))
+name_student = []
+second_name_student = []
+score = []
+
+for i in range(num_student):
+    name_student.append(input("Введите имя студента: "))
+    second_name_student.append(input("Введите фамилию студента: "))
+    score.append(int(input("Введите оценку студента: ")))
+    print("Студент", name_student[i], second_name_student[i], "с оценкой", score[i], "добавлен.")
+
+avg = sum(score) / num_student
+
+print("Средний балл", round(avg), ". Студенты с баллом выше среднего: ")
+for i in range(num_student):
+    if avg < score[i]:
+        print(name_student[i] + second_name_student[i] + "\n")
+    else:
+        continue
