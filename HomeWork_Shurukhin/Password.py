4a45b6de2e0fdd35c549e03e750c08168b989cfd
Pr0igroman <ilya-shuruhin@yandex.ru>
2024-03-25 01:36:03 +0300

    HomeWork Password

---
+++ HomeWork_Shurukhin/Password.py
@@ -0,0 +1,13 @@
+import re
+
+reg = r"^[0-9a-zA-Z@_-]{6,18}$"
+
+password1 = "my-p@ssw0rd"
+password2 = "eerge"
+password3 = "Parol_123-456@"
+password4 = "passwordpassword123456"
+
+print(re.findall(reg, password1))
+print(re.findall(reg, password2))
+print(re.findall(reg, password3))
+print(re.findall(reg, password4))
