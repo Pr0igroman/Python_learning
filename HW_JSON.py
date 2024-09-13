8c9c8f8b4ae127e6816a42e994b31e7321a1e841
Pr0igroman <ilya-shuruhin@yandex.ru>
2024-09-14 02:52:28 +0300

    HW_JSON.py

---
+++ HW_JSON.py
@@ -0,0 +1,39 @@
+import json
+from random import choice
+
+
+def gen_person():
+    name = ''
+    tel = ''
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+    while len(name) != 7:
+        name += choice(letters)
+
+    while len(tel) != 10:
+        tel += choice(nums)
+
+    person = {
+        'name': name,
+        'tel': tel
+    }
+
+    return person
+
+
+def write_json(person_dict):
+    try:
+        with open('persons.json', 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+
+    data[person_dict["tel"]] = person_dict
+    with open("persons.json", "w") as f:
+        json.dump(data, f, indent=2)
+
+
+for i in range(100):
+    write_json(gen_person())
